2cf5509a0e38c9f51e94dd6c9dc6edecca92c640
razdvapoka <razdvapoka@gmail.com>
2019-05-27 22:07:55 +0400

    :tada: Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import 'defaults.css';
+import './style';
+import App from './components/app';
+
+export default App;
---
+++ src/routes/home/index.js
@@ -0,0 +1,10 @@
+import { h } from 'preact';
+import style from './style.styl';
+
+const Home = () => (
+	<div class={style.home}>
+		<h1>Hi there!</h1>
+	</div>
+);
+
+export default Home;
